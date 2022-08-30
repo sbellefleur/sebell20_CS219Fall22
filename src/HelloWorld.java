374a14432e2ebd4143222d6eb836fe225ecc5eed
sarahbellefleur <sbellefleur327@gmail.com>
2022-08-30 15:13:59 -0400

    Update to include today's changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,20 @@
 //by convention, class na,es are capitalized.
 public class HelloWorld {
+
+    /*
+    This is a multi line block comment
+    Another comment line.
+     */
     public static void main (String [] args) {
         System.out.println("Hello World");
 
         int x = 5;
         boolean z = true;
         double pi = 3.14159;
+        String name = "CS219 Techniques of Computer Science";
+        double avogadros = 6.022140857e23; //Scientific notation is universal across languages
+        double electron_mass = 9.109e-31;
+
 
     }
 }
